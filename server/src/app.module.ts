ecf320f5bb388a87bb10fbb442b47a4b7f018aac
sachin saini <sachinsaini18585@gmail.com>
2026-06-03 17:43:14 +0530

    Fix repository structure

---
+++ server/src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { PrismaModule } from './prisma/prisma.module';
+
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { CafesModule } from './modules/cafes/cafes.module';
+import { ReviewsModule } from './modules/reviews/reviews.module';
+import { FavoritesModule } from './modules/favorites/favorites.module';
+import { RecommendationsModule } from './modules/recommendations/recommendations.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    PrismaModule,
+
+    AuthModule,
+    UsersModule,
+    CafesModule,
+    ReviewsModule,
+    FavoritesModule,
+    RecommendationsModule,
+  ],
+})
+export class AppModule {}
---
+++ server/src/modules/cafes/cafes.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('cafes')
+export class CafesController {}
